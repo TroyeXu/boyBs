3453c92e6adb0674cd605df247a02b73177b1fb9
TroyeXu <zooka8548@gmail.com>
2018-02-23 20:18:57 +0800

    123

--- public.1/js/all.js
+++
@@ -1,2 +0,0 @@
-"use strict";(new WOW).init(),$(document).ready(function(){$(".scrollTop").click(function(t){t.preventDefault();var a=$(this).attr("href"),e=$(a).offset().top;$("html, body").animate({scrollTop:e+200},1e3)})}),window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments)}gtag("js",new Date),gtag("config","UA-89388320-3");
-//# sourceMappingURL=all.js.map
