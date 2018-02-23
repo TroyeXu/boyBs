351d365c5ef57261637297dc96d3341ffc4ca6d4
TroyeXu <zooka8548@gmail.com>
2018-02-23 15:54:33 +0800

    fiximgga

--- sourse/js/all.js
+++ sourse/js/all.js
@@ -13,5 +13,4 @@ $(document).ready(function () {
 window.dataLayer = window.dataLayer || [];
 function gtag() { dataLayer.push(arguments); }
 gtag('js', new Date());
-
 gtag('config', 'UA-89388320-3');
